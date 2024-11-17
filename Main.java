44464422c2d16cb8cffd5d9822bfd82ae1743ace
anoubl <amineanouble@gmail.com>
2024-11-17 15:55:49 +0100

    java main

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    // Méthode principale
+    public static void main(String[] args) {
+        // Affiche un message de bienvenue
+        System.out.println("Bonjour, bienvenue dans le programme Java!");
+
+        // Exemple d'utilisation : Calcul de la somme de deux nombres
+        int a = 5;
+        int b = 10;
+        int somme = addition(a, b);
+
+        System.out.println("La somme de " + a + " et " + b + " est : " + somme);
+    }
+
+    // Méthode auxiliaire pour additionner deux nombres
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+}
